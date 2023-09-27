481285967c295f4f8cbba1f92eb105b9a22a78a9
Akileshkrishnan <120013200+akileshkrishnan@users.noreply.github.com>
2023-09-27 13:07:24 +0530

    Add files via upload

---
+++ day 10/com/akilesh_hospital_management_system7/HospitalManagementSystem7Application.java
@@ -0,0 +1,13 @@
+package com.akilesh_hospital_management_system7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementSystem7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementSystem7Application.class, args);
+	}
+
+}
