60688d40530a263006f4cdfd8cdaf9687587085d
Akileshkrishnan <120013200+akileshkrishnan@users.noreply.github.com>
2023-10-05 12:08:34 +0530

    Add files via upload

---
+++ day 2s/HospitalManagementSystem7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akilesh.hospital_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystem7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
